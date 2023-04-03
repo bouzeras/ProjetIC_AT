91f58d746f58b0dde4231f6f516ce02bb87f58fd
bouzeras <79795451+bouzeras@users.noreply.github.com>
2023-04-03 21:53:07 +0200

    Delete group.py

--- group.py
+++
@@ -1,7 +0,0 @@
-class Group:
-    def __init__(self, group_name, description, members, access_permissions, privacy_settings):
-        self.group_name = group_name
-        self.description = description
-        self.members = members
-        self.access_permissions = access_permissions
-        self.privacy_settings = privacy_settings
